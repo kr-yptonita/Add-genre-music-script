bdc42b666d33280c72dba24fe989c892d4788450
kryptonita <kr.iptonita@outlook.con>
2026-02-21 00:03:29 -0600

    Add logo

--- secrets.py
+++ secrets.py
@@ -1,12 +1,12 @@
 # secrets.py
 
 # Spotify (https://developer.spotify.com/dashboard)
-CLIENT_ID = ''
-CLIENT_SECRET = ''
+CLIENT_ID = '22266446177d4208be5f202f03d0f842'
+CLIENT_SECRET = '3cf9d5e9f77d4c6bbf9be74b691a6978'
 
 # Last.fm (https://www.last.fm/api/account/create)
-LASTFM_API_KEY = ''
-LASTFM_API_SECRET = ''
+LASTFM_API_KEY = '5162c8f27977af7248c5e55a9dd73607'
+LASTFM_API_SECRET = '9ad36c4e8d7ffab7bfd17acc1f53d48b'
 
 # Discogs (https://www.discogs.com/settings/developers → Generate token)
-DISCOGS_USER_TOKEN = ''
+DISCOGS_USER_TOKEN = 'CIQXoRviKntkwyiisbuteWGuYYuGdzAV'
